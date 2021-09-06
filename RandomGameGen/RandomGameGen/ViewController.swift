8b01f05f99892a9d092fd5c4d7547303ebf26200
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2021-09-06 09:25:20 +0200

    layout

--- RandomGameGen/RandomGameGen/ViewController.swift
+++ RandomGameGen/RandomGameGen/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var gamelabel: UILabel!
+    @IBOutlet weak var shufflebutton: UIButton!
+    
+    
+    let images: [UIImage] = [#imageLiteral(resourceName: "modernWarfare"),#imageLiteral(resourceName: "rocketLeague"),#imageLiteral(resourceName: "csGO"),#imageLiteral(resourceName: "overwatch"),#imageLiteral(resourceName: "fortnite"),#imageLiteral(resourceName: "minecraft"),#imageLiteral(resourceName: "gta5"),#imageLiteral(resourceName: "valorant"),#imageLiteral(resourceName: "rocketLeague"),#imageLiteral(resourceName: "leagueOfLegends"),#imageLiteral(resourceName: "apex")]
+    
+    @IBAction func shufflebuttonaction(_ sender: UIButton) {
+        imageview.image = images.randomElement()
+    }
 }
 
