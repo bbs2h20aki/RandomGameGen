5cb2eb2c566e6810ffb9955af3b0e70271969188
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2021-09-06 08:35:00 +0200

    initial commit

---
+++ RandomGameGen/RandomGameGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomGameGen
+//
+//  Created by Fabian Kirchmann / BBS2H20A on 06.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
